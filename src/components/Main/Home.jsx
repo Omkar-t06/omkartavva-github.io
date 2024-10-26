e033f50a4901928a5eca83bad86885a2bdbbd012
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-10-27 01:14:46 +0530

    Build Home Section

--- src/components/Main/Home.jsx
+++ src/components/Main/Home.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionalCards from './SectionalCards'
+import ReactTypingEffect from 'react-typing-effect'
 
 function Home() {
+
   return (
     <SectionalCards sectionId={'home'}>
-      Home
+      <section className="flex flex-col lg:flex-row-reverse lg:justify-between space-y-6 lg:space-y-0">
+        <section className="flex justify-center shadow-lg shadow-orange-300 dark:shadow-slate-300 rounded-full border-solid border-off-white dark:border-light-cyan hover:border-orange-100 dark:hover:border-charcoal border-8">
+          <img 
+            src="https://user-images.githubusercontent.com/37551474/113611467-3a567d80-9657-11eb-862b-b07b4f105c6f.gif" 
+            alt="profile"
+            className="object-cover rounded-full h-48 w-48 lg:h-64 lg:w-64"
+          />
+        </section>
+        <section className="text-center lg:text-left lg:w-1/2 text-3xl font-bold space-y-4">
+          <h1 className='text-4xl'>Hi, I'm Omkar Tavva</h1>
+          <ReactTypingEffect
+            text={["An Engineering Student", "A Flutter Developer", "A Programmer", "A Tech Enthusiast"]}
+            speed={150}
+            eraseSpeed={100}
+          />
+          <p className='text-lg text-gray-700 dark:text-gray-300 mt-4'>
+            Welcome to my personal site! I’m passionate about creating engaging and functional applications that solve real-world problems. Let’s build something amazing together!
+          </p>
+          <section className='mt-6 flex lg:justify-start justify-center'>
+          <a 
+            href="#about"
+            className="mr-4 bg-teal-500 text-xl text-white py-2 px-4 rounded-lg shadow hover:bg-teal-600 transition"
+          >
+            Learn More About Me
+          </a>
+          <a
+            href="/path-to-resume/Omkar_Resume.pdf" 
+            download 
+            className="bg-blue-500 text-xl text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600 transition"
+          >
+            Download My Resume
+          </a>
+          </section>
+        </section>
+      </section>
     </SectionalCards>
   )
 }
--- src/components/Main/SectionalCards.jsx
+++ src/components/Main/SectionalCards.jsx
@@ -4,7 +4,7 @@ function SectionalCards({sectionId,children}) {
   return (
     <section 
       id = {sectionId} 
-      className = 'm-4 p-4 rounded-md shadow-md h-60 dark:border-charcoal dark:border-solid dark:border-2'
+      className = 'm-4 p-4 rounded-md shadow-md dark:border-charcoal dark:border-solid dark:border-2'
     >
       {children}
     </section>
